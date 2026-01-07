c56748770bcf7174ce6d4e11e19c2be4b53e0b82
Jithin <jithinilip@gmail.com>
2026-01-08 07:46:18 +0800

    Create js/config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// /js/config.js
+// Put your Supabase project keys here (public keys are safe to expose in frontend)
+
+export const SUPABASE_URL = "https://czlmeehcxrslgfvqjfsb.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6bG1lZWhjeHJzbGdmdnFqZnNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1MzU0NjgsImV4cCI6MjA4MzExMTQ2OH0.vHeIA2n6tm3F3IEoOPBsrIXQ1JXRlhe6bU4VP9b2lek";
+
+// Routes
+export const ROUTES = {
+  login: "/auth/login.html",
+  dashboard: "/dashboard/",
+  setup: "/setup/profile-setup.html",
+};
